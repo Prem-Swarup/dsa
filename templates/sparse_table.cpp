439e2e6816f8f0b75982d02bf244550dee894aa7
Prem Swarup <premswarup565@gmail.com>
2023-10-11 23:58:57 +0530

    Create sparse_table.cpp

---
+++ templates/sparse_table.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+
+template<typename T=long long>
+T func(T a, T b){
+  
+  return a+b;
+}
+
+template<typename T=long long>
+class SparseTable {
+private:
+    std::vector<std::vector<T>> table;
+
+public:
+    SparseTable(const std::vector<T>& arr) {
+        T n = arr.size();
+        T logN = log2(n) + 1;
+        table.resize(n, std::vector<T>(logN));
+
+        // Initialize the table with values from the original array
+        for (T i = 0; i < n; i++) {
+            table[i][0] = arr[i];
+        }
+
+        // Build the sparse table
+        for (T j = 1; (1 << j) <= n; j++) {
+            for (T i = 0; i + (1 << j) - 1 < n; i++) {
+                table[i][j] = func(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
+            }
+        }
+    }
+
+    T query(T left, T right) {
+        T sum = 0;
+
+        for (T j = log2(right - left + 1); j >= 0; j--) {
+            if ((1 << j) <= (right - left + 1)) {
+                sum += table[left][j];
+                left += (1 << j);
+            }
+        }
+
+        return sum;
+    }
+
+    T query2(T L, T R) {
+      int j = (int)log2(R - L + 1);
+      return func(table[L][j], table[R - (1 << j) + 1][j]);
+    }
+      
+};
+
+int main() {
+    std::vector<long long> arr = {1, 3, 5, 7, 9, 11, 13, 15};
+
+    SparseTable st(arr);
+
+    // Example range queries
+    std::cout << "Range sum from index 2 to 5: " << st.query(2, 5) << std::endl;
+    std::cout << "Range sum from index 1 to 7: " << st.query(1, 7) << std::endl;
+
+    return 0;
+}
